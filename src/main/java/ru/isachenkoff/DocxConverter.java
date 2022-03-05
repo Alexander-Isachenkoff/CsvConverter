50eafc44dcd2da38854ea556c04f3ac9deac94f6
isachenkoff <isachenkoff.alexander@yandex.ru>
2022-03-05 12:49:19 +0800

    docx all tables parsing

--- src/main/java/ru/isachenkoff/DocxConverter.java
+++ src/main/java/ru/isachenkoff/DocxConverter.java
@@ -3,11 +3,13 @@ package ru.isachenkoff;
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFTable;
+import org.apache.poi.xwpf.usermodel.XWPFTableRow;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,13 +18,25 @@ public class DocxConverter extends AbstractConverter {
     public List<Pair<String, String>> convert(File file) throws IOException {
         InputStream fs = new FileInputStream(file);
         XWPFDocument document = new XWPFDocument(fs);
-        XWPFTable tbl = document.getTables().get(0);
-        String result = tbl.getRows().stream()
-                .map(row -> row.getTableCells().stream()
-                        .map(cell -> quoteIfContainsSeparator(cell.getText()))
-                        .collect(Collectors.joining(getSeparator()))
-                )
+        List<Pair<String, String>> results = new ArrayList<>();
+        List<XWPFTable> tables = document.getTables();
+        for (int i = 0; i < tables.size(); i++) {
+            XWPFTable table = tables.get(i);
+            String result = processTable(table);
+            results.add(Pair.of(FileUtils.getName(file) + i, result));
+        }
+        return results;
+    }
+    
+    private String processTable(XWPFTable table) {
+        return table.getRows().stream()
+                .map(this::processRow)
                 .collect(Collectors.joining(System.lineSeparator()));
-        return List.of(Pair.of(FileUtils.getName(file), result));
+    }
+    
+    private String processRow(XWPFTableRow row) {
+        return row.getTableCells().stream()
+                .map(cell -> quoteIfContainsSeparator(cell.getText()))
+                .collect(Collectors.joining(getSeparator()));
     }
 }
